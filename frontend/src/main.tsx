fafda5c68e3495da1497357658b9e7ef330c9210
kpranat <pranatkheria@gmail.com>
2026-01-28 18:53:59 +0530

    fixed email error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext'
+import { initEmailJS } from './services/emailService'
+
+// Initialize EmailJS
+initEmailJS();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
